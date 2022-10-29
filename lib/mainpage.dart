e5d666cc0415ff4d76161eadb1823b70993abf1b
ZEYNEP <zeynepclktn@gmail.com>
2022-10-29 23:37:54 +0300

    added old basket text button

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -14,14 +14,14 @@ class _MainpageState extends State<Mainpage> {
       body: Padding(
         padding:const EdgeInsets.all(25.0),
         child:Column (
-          mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
             children:[
               Image.asset('assets/logo.jpg'),
               Container(
                 width:300.0,
-              child:TextField(
+              child:const TextField(
               decoration: InputDecoration(
-                suffixIcon: const Icon(Icons.search),
+                suffixIcon: Icon(Icons.search),
               labelStyle: TextStyle(
                   color: Colors.grey,
                   fontSize: 16.0),
@@ -30,9 +30,21 @@ class _MainpageState extends State<Mainpage> {
                   borderRadius: BorderRadius.all(Radius.circular(30)),
                   gradient: LinearGradient(
                       colors:[Colors.deepPurpleAccent, Colors.purple])),
-        ),),
-              )
-
+              ),
+              ),
+              ),
+              TextButton.icon(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.white,
+                    textStyle: const TextStyle(
+                        fontSize: 12,
+                        fontStyle: FontStyle.normal),
+                        shadowColor: Colors.purple,
+                  ),
+                  label:Text('Geçmiş Sepetler',style:TextStyle(color:Colors.purple)),
+                  icon:Icon(Icons.shopping_basket_outlined, color: Colors.purple,),
+              ),
 
   ]
 
