0d971146d14ec2b585abc28416cb7859a7119a0f
ZEYNEP <zeynepclktn@gmail.com>
2022-10-29 22:57:47 +0300

    added search box and logo image in mainpage

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gradient_borders/gradient_borders.dart';
 
 class Mainpage extends StatefulWidget {
   @override
@@ -9,13 +10,34 @@ class _MainpageState extends State<Mainpage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          "Mainpage!",
-          style: TextStyle(fontSize: 20, color: Colors.white),
-        ),
-      ),
-      backgroundColor: Colors.blue[700],
+        backgroundColor: Colors.white,
+      body: Padding(
+        padding:const EdgeInsets.all(25.0),
+        child:Column (
+          mainAxisAlignment: MainAxisAlignment.center,
+            children:[
+              Image.asset('assets/logo.jpg'),
+              Container(
+                width:300.0,
+              child:TextField(
+              decoration: InputDecoration(
+                suffixIcon: const Icon(Icons.search),
+              labelStyle: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 16.0),
+              border:GradientOutlineInputBorder(
+                width: 3.0,
+                  borderRadius: BorderRadius.all(Radius.circular(30)),
+                  gradient: LinearGradient(
+                      colors:[Colors.deepPurpleAccent, Colors.purple])),
+        ),),
+              )
+
+
+  ]
+
+      )
+     )
     );
   }
 }
