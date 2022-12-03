5a2d050f96c352cf2e3b64dbd8fe1af4598f1413
BurakAndarman <brkandrmn@hotmail.com>
2022-12-03 16:10:43 +0300

    scraping methods created

--- lib/pages/mainpage.dart
+++ lib/pages/mainpage.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:gradient_borders/gradient_borders.dart';
+import 'package:html/dom.dart';
 import 'package:techpack/pages/categories.dart';
+import 'package:html/parser.dart' as parser;
+import 'package:http/http.dart' as http;
+
+import '../models/product_model.dart';
 
 class Mainpage extends StatefulWidget {
   @override
@@ -8,6 +13,185 @@ class Mainpage extends StatefulWidget {
 }
 
 class _MainpageState extends State<Mainpage> {
+  String queryBuilder(String query, String store) {
+    String url;
+    if (store == "media markt") {
+      url =
+          "https://www.mediamarkt.com.tr/tr/search.html?query=$query&searchProfile=onlineshop&channel=mmtrtr&outlet=1771";
+    } else if (store == "teknosa") {
+      url = "https://www.teknosa.com/arama/?s=$query";
+    } else if (store == "itopya") {
+      url = "https://www.itopya.com/AramaSonuclari?text=$query";
+    } else {
+      url = "https://www.vatanbilgisayar.com/arama/$query/";
+    }
+
+    return url;
+  }
+
+  List<ProductModel> scraper(String store, Document document) {
+    List<ProductModel> searchedProducts=[];
+
+    if (store == "media markt") {
+      var image = document
+          .getElementsByClassName("mm-products")[0]
+          .children[0]
+          .children[0]
+          .children[1]
+          .children[0]
+          .children[0]
+          .attributes["src"];
+
+      var image1 = document
+          .getElementsByClassName("mm-products")[0]
+          .children[0]
+          .children[1]
+          .children[1]
+          .children[0]
+          .children[0]
+          .attributes["src"];
+
+      var image2 = document
+          .getElementsByClassName("mm-products")[0]
+          .children[0]
+          .children[2]
+          .children[1]
+          .children[0]
+          .children[0]
+          .attributes["src"];
+
+      var image3 = document
+          .getElementsByClassName("mm-products")[0]
+          .children[0]
+          .children[3]
+          .children[1]
+          .children[0]
+          .children[0]
+          .attributes["src"];
+
+      var title = document
+          .getElementsByClassName("mm-products")[0]
+          .children[0]
+          .children[0]
+          .children[2]
+          .children[0]
+          .children[1]
+          .children[0];
+
+      var title1 = document
+          .getElementsByClassName("mm-products")[0]
+          .children[0]
+          .children[1]
+          .children[2]
+          .children[0]
+          .children[1]
+          .children[0];
+
+      var title2 = document
+          .getElementsByClassName("mm-products")[0]
+          .children[0]
+          .children[2]
+          .children[2]
+          .children[0]
+          .children[1]
+          .children[0];
+
+      var title3 = document
+          .getElementsByClassName("mm-products")[0]
+          .children[0]
+          .children[3]
+          .children[2]
+          .children[0]
+          .children[1]
+          .children[0];
+
+      var price = document
+          .getElementsByClassName("mm-products")[0]
+          .children[0]
+          .children[0]
+          .children[2]
+          .children[1]
+          .children[0]
+          .children[0]
+          .attributes["data-price"];
+
+      var price1 = document
+          .getElementsByClassName("mm-products")[0]
+          .children[0]
+          .children[1]
+          .children[2]
+          .children[1]
+          .children[0]
+          .children[0]
+          .attributes["data-price"];
+
+      var price2 = document
+          .getElementsByClassName("mm-products")[0]
+          .children[0]
+          .children[2]
+          .children[2]
+          .children[1]
+          .children[0]
+          .children[0]
+          .attributes["data-price"];
+
+      var price3 = document
+          .getElementsByClassName("mm-products")[0]
+          .children[0]
+          .children[3]
+          .children[2]
+          .children[1]
+          .children[0]
+          .children[0]
+          .attributes["data-price"];
+
+      searchedProducts.addAll([
+        ProductModel(
+            title: title.text,
+            category: "",
+            price: int.parse(price!),
+            vendor: "media markt",
+            id: 0,
+            image: image!),
+        ProductModel(
+            title: title1.text,
+            category: "",
+            price: int.parse(price1!),
+            vendor: "media markt",
+            id: 1,
+            image: image1!),   
+        ProductModel(
+            title: title2.text,
+            category: "",
+            price: int.parse(price2!),
+            vendor: "media markt",
+            id: 2,
+            image: image2!),
+        ProductModel(
+            title: title3.text,
+            category: "",
+            price: int.parse(price3!),
+            vendor: "media markt",
+            id: 3,
+            image: image3!)
+      ]);
+    }
+  }
+
+  Future<List<String>> extractData(String query, String store) async {
+    final response =
+        await http.Client().get(Uri.parse(queryBuilder(query, store)));
+
+    if (response.statusCode == 200) {
+      var document = parser.parse(response.body);
+      try {} catch (e) {
+        return ['', '', 'ERROR!'];
+      }
+    } else {
+      return ['', '', 'ERROR: ${response.statusCode}.'];
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,7 +213,8 @@ class _MainpageState extends State<Mainpage> {
                               onPressed: () {
                                 Navigator.push(
                                   context,
-                                  MaterialPageRoute(builder: (context) => const Categories()),
+                                  MaterialPageRoute(
+                                      builder: (context) => const Categories()),
                                 );
                               },
                               icon: const Icon(Icons.shopping_basket,
@@ -49,7 +234,8 @@ class _MainpageState extends State<Mainpage> {
                   onSubmitted: (value) {
                     Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => const Categories()),
+                      MaterialPageRoute(
+                          builder: (context) => const Categories()),
                     );
                   },
                   decoration: const InputDecoration(
@@ -67,8 +253,8 @@ class _MainpageState extends State<Mainpage> {
                 onPressed: () {},
                 style: ElevatedButton.styleFrom(
                   primary: Colors.white,
-                  textStyle:
-                  const TextStyle(fontSize: 12, fontStyle: FontStyle.normal),
+                  textStyle: const TextStyle(
+                      fontSize: 12, fontStyle: FontStyle.normal),
                   shadowColor: Colors.purple,
                 ),
                 label: const Text('Geçmiş Sepetler',
@@ -79,7 +265,8 @@ class _MainpageState extends State<Mainpage> {
                 ),
               ),
             ],
-          ),),
+          ),
+        ),
       ),
     );
   }
